31e44bc5f8360dd66d6045c07be046419141de30
Swift <swift@mbp2017.local>
2020-02-24 14:47:30 +0800

    Install Modules and Setting

--- api/index.js
+++ api/index.js
@@ -1,7 +1,30 @@
 const express = require('express')
+const bodyParser = require('body-parser');
+const session = require('express-session');
+const MySQLStore = require('express-mysql-session')(session);
+require('dotenv').config();
 
 // Create express instance
 const app = express()
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use(express.static(__dirname+'/public'));
+
+app.use(session({
+  secret:'keyboard cat',
+  resave:false,
+  saveUninitialized:true,
+  store: new MySQLStore({
+    host: process.env.DB_HOST,
+    port: 3306,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME,
+    expiration: 86400000 * 7,
+  }),
+}));
 
 // Require API routes
 const users = require('./routes/users')
